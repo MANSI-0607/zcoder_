cb6558c4246ddd768e50de832bf95541822c6db5
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-02 01:06:58 +0530

    created sign in sign up pages

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import MyStack from "./screen/MyStack";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Explore from "./screen/Explore";
 import Footer from "./components/Footer";
+import Login from "./screen/Login";
+import SignUp from "./screen/SignUp";
 function App() {
   return (
     <React.Fragment>
       <Router>
         <Header />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          {/* <Route path="/explore" component={Explore} /> */}
+          <Route exact path="/home" element={<Home />} />
           <Route path="/addquestion" element={<AddQuestion />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/mystack" element={<MyStack />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/" element={<SignUp />} />
 
           <Route path="*" element={<Error />} />
         </Routes>
--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import './Login.css'; // Import the CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-export default Login
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const Navigate=useNavigate();
+  const handleSignup=()=>{
+    Navigate("/")
+  }
+
+  return (
+    <div className="login">
+      <div className="login-container">
+        <h2>Login</h2>
+        <label>Username</label>
+        <input type="text" className="login-input" />
+        <label>Password</label>
+        <div className="password-container">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            className="login-input"
+          />
+          <FontAwesomeIcon
+            icon={passwordVisible ? faEyeSlash : faEye}
+            className="eye-icon"
+            onClick={togglePasswordVisibility}
+          />
+        </div>
+        <button className="login-button">Login</button>
+        <div className="not-member">
+          <h3>Not a member?</h3>
+          <button className="signup-button" onClick={handleSignup}>Signup now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
