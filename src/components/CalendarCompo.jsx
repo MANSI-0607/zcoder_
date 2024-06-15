074d27f2e2f7cc75a83fc184ab347815bba9cb65
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-16 00:16:19 +0530

    CODEFORCES

--- src/components/CalendarCompo.jsx
+++ src/components/CalendarCompo.jsx
@@ -4,13 +4,16 @@ const CalendarCompo = (props) => {
   const style = {
     backgroundColor: props.background
   };
-  
 
   const { contest } = props;
   const startDate = new Date(contest.startTimeSeconds * 1000);
 
+  const handleClick = () => {
+    window.location.href = "https://codeforces.com/contests";
+  };
+
   return (
-    <div className="CalendarCompo" style={style}  href="codeforces.com">
+    <div className="CalendarCompo" style={style} onClick={handleClick}>
       <p className="contest-name">{contest.name}</p>
       <p className="contest-date">{startDate.toLocaleDateString()}</p>
       <p className="contest-time">{startDate.toLocaleTimeString()}</p>
