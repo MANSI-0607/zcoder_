54702cba75c29a13092f0643035399764cff8f20
pabitrakumariitg <pabitrakumar50abc@gmail.com>
2024-06-15 17:54:24 +0530

    m

--- src/components/YourPost.jsx
+++ src/components/YourPost.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import { Container } from "@mui/material";
 import like from "../assets/like.svg"; // Adjust path as per your file structure
 import view from "../assets/view.svg"; // Adjust path as per your file structure
-import "../screen/MyStack.css"
+import "../screen/MyStack.css";
 const YourPost = ({ item }) => {
   const accessColorClass = item.access === "public" ? "public" : "private";
-
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+    return date.toLocaleString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
   return (
     <div className="yourpost">
       <Container>
         <span className="postdatetime">
-          {new Date(item.timeOfCreation).toLocaleString()}
+          {formatDate(item.timeOfCreation)}
+  
         </span>
         <div className="yr_ques">{item.question}</div>
         <div className="bottomyrpost">
-          <div >
+          <div>
             <p className={accessColorClass}>{item.access}</p>
           </div>
 
